ab60ced264c8393f0e835b0870697ee5c68f0c46
eric <eric.brilliant@gmail.com>
2026-04-05 14:15:03 +0800

    py recover ok

--- start.bat
+++ start.bat
@@ -5,4 +5,4 @@ echo Server will be available at: http://127.0.0.1:5001
 echo Press Ctrl+C to stop the server
 echo.
 start http://127.0.0.1:5001
-py app.py
+"C:\Users\ng\AppData\Local\Programs\Python\Python311\python.exe" app.py
